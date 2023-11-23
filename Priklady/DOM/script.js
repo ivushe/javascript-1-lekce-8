e026f3fd8f91bb5aed564ddaf1a8c278cd90248a
Michal Kucera <m-kucera@outlook.com>
2023-11-23 16:26:12 +0100

    podklady

---
+++ Priklady/DOM/script.js
@@ -0,0 +1,39 @@
+/*
+Úkoly v tomto cvičení:
+
+1) JavaScriptem změň text nadpisu.
+	
+2) JavaScriptem změnit barvu nadpisu.
+	 (CSS vlastnost: color)
+
+3) Zvětšit velikost písma odstavce a/nebo ho udělat tučně nebo kurzívou.
+	 (CSS vlastnosti: font-size, font-weight, font-style)
+
+4) Vyměnit obrázek kočky za psa. Koček je všude dost, nepotřebujeme tu další.
+	 (HTML atribut: src)
+
+5) Přesunout čtvereček na jiné místo nebo ho otočit.
+	 (nastav CSS vlastnost position na hodnotu 'absolute' a pak můžeš měnit
+	 pozici prvku pomocí CSS vlastností top a left)
+
+6) Vytvoř v CSS třídu s nějakými CSS vlastnostmi a zkuš ji přidat na libovolný
+	 prvek na stránce.
+	 (v CSS nastav třídě třeba barvu pozadí.)
+
+
+Nápověda:
+- V HTML přidej k elementu atribut id="neco" a nějak si prvek pojmenuj, aby se ti lépe vybíral
+- V JavaScriptu najdi daný HTML element pomocí document.querySelector("#neco") a ulož si ho
+	do proměnné, se kterou budeš nadále pracovat
+- Text prvku změníš pomocí prvek.textContent = "hodnota";
+- HTML atributy nalezeného prvku změníš pomocí prvek.attribut = "hodnota";
+- CSS vlastnosti nalezeného prvku změníš pomocí prvek.style.cssVlastnost = "hodnota";
+- Vzpomeň si, jak se jmenují CSS vlastnosti pro barvu, písmo, atd.
+	Víceslovné názvy vlastností (ty s pomlčkou) přepíšu v JavaScriptu do tzv. camelCase.
+	Např. z background-color v CSS se v JavaScriptu stane backgroundColor.
+- CSS třídy můžeš na elementy přidávat pomocí prvek.classList.add = "csstrida";
+  Třídy můžeš z elementu i odebrat pomocí metody remove nebo přepínat třídu metodou toggle.
+*/
+
+// tady je místo pro tvůj program
+
---
+++ Priklady/Eventy/script.js
@@ -0,0 +1,26 @@
+/*
+Úkoly v tomto cvičení:
+1) Při kliknutí myší na čtvereček ho otoč o 45 stupňů.
+	 Budeš potřebovat událost click, vybrat čtvereček
+	 na stránce a změnit mu příslušné vlastnosti.
+
+2) Zkus čtvereček při druhém kliknutí zase vrátit zpátky.
+	 Buď si musíš někde poznačit, je-li čtvereček otočený nebo ne
+	 a podle toho mu správně měnit vlastnosti.
+	 Nebo můžeš otočení čtverečku nastavit v CSS třídě, kterou budeš
+	 na element přidávat nebo z něho odebírat. Když použijet pro
+	 přepínání třídy metodu toggle, máš vše skoro bez práce.
+	 Vyber si postup, který tobě připadá nejlépe pochopitelný.
+
+3) Při stisknutí klávesy změň čtverečku barvu.
+	 Budeš potřebovat událost keydown. Mysli na to, že tyto události
+	 nelze nastavovat konkrétním elementům, ale musíš je nastavit pro
+	 celou stránku na elementu <body>. (Je to logické. Jak bys stiskla
+	 klávesu jen nad konkrétním elementem?)
+
+	 Pokud chceš reagovat na konkrétní klávesu (a ne na stisk libovolné),
+	 budeš si muset do obslužné funkce pro událost předat i objekt event,
+	 který má vlastnost key, ve které je kód stisknuté klávesy.
+*/
+
+// tady je místo pro tvůj program
